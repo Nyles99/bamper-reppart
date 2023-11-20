9aa85be70d93eedd8ebbee1ed3f053d0d95e508c
Никита Молчанов <nylesszpg@gmail.com>
2023-11-20 14:59:24 +0300

    url

--- main.py
+++ main.py
@@ -16,15 +16,13 @@ headers = {
 }
 
 url = "https://bamper.by/zchbu/god_2023-2023/store_Y/isused_Y/"
-
-service = Service(executable_path="C:\\Dev\\parsingselenium\\chromedriver\\chromedriver.exe")
-options = webdriver.ChromeOptions()
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36")
-driver = webdriver.Chrome(service=service, options=options)
+driver = webdriver.Chrome()
 
 driver.get(url=url)
 time.sleep(2)
 
-with open(f"{url}.html", "w", encoding="utf-8") as file:
+with open("index.html", "w", encoding="utf-8") as file:
     file.write(driver.page_source)
+
+driver.close()
+driver.quit()
