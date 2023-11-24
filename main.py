3227a5a84a522e0fd710fa3fdcaa5002b7d0e53e
Никита Молчанов <nylesszpg@gmail.com>
2023-11-24 11:53:40 +0300

    no foto

--- main.py
+++ main.py
@@ -22,6 +22,29 @@ options.add_experimental_option('excludeSwitches', ['enable-logging'])
 options.add_argument('--ignore-certificate-errors')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
+with open(f"data_bamper.csv", "w", encoding="utf-8") as file_data:
+    writer = csv.writer(file_data)
+
+    writer.writerow(
+        (
+            "АРТИКУЛ",
+            "МАРКА",
+            "МОДЕЛЬ",
+            "ГОД",
+            "ТОПЛИВО",
+            "ОБЪЕМ",
+            "ТИП ДВИГАТЕЛЯ",
+            "КОРОБКА",
+            "ТИП КУЗОВА",
+            "ЗАПЧАСТЬ",
+            "ОПИСАНИЕ",
+            "ПОД ЗАКАЗ",
+            "ЦЕНА",
+            "НОВАЯ",
+            "ФОТО",
+        )
+    )
+
 driver.get(url=url)
 time.sleep(2)
 
@@ -64,7 +87,7 @@ for number, number_href in part_href_url.items():
         src = file.read()
 
     soup = BeautifulSoup(src, "lxml")
-    """price_obj = soup.find_all("meta", itemprop="price")
+    price_obj = soup.find_all("meta", itemprop="price")
     # print (price_obj)
     for item_price in price_obj:
         price = item_price.get("content")
@@ -81,22 +104,103 @@ for number, number_href in part_href_url.items():
         marka = model_and_year[: model_and_year.find(" ")]
         model = model_and_year[model_and_year.find(" ")+1 : model_and_year.find(",")]
         year = model_and_year[model_and_year.find(",")+2 : model_and_year.find("г.")]
-    print(marka, model, year, price, number_href)"""
+    #print(marka, model, year, price, number_href)
 
-    """artical_obj = soup.find_all("span", class_="data-type f13")
+    artical_obj = soup.find_all("span", class_="data-type f13")
     for item_artical in artical_obj:
-        artical = item_artical.text"""
-    
-    """info_obj = soup.find_all("span", class_="media-heading cut-h-375")
+        artical = item_artical.text
+    order = "Нет информации"
+    status = "Б/у"
+    info_obj = soup.find_all("span", class_="media-heading cut-h-375")
     for item_info in info_obj:
-        info = item_info.text.replace("  ","").replace("\n","")"""
+        info = item_info.text.replace("  ","").replace("\n","")
+        info_lower = info.lower()
+        if "ПОД ЗАКАЗ" in info:
+            order = "ПОД ЗАКАЗ"
+        print(info)
+        if "новый" in info_lower:
+            status = "Новая"
+        elif "новая" in info_lower:
+            status = "Новая"
+        elif "новые" in info_lower:
+            status = "Новые"
+    print(status)
+    #print(order)        
+    #print(info)
     
     benzik_obj = soup.find_all("div", style="font-size: 17px;")
+    fuel = None
+    transmission = "Нет информации"
+    engine = "Нет информации"
+    volume = None
+    car_body = None
     # print(benzik_obj)
     for item_benzik in benzik_obj:
         benzik = None
         benzik = item_benzik.text.replace("  ","").replace("\n","")
-    print(benzik)
+        if "л," in benzik:
+            volume = benzik[benzik.find("л,") - 5 : benzik.find("л,") + 1]
+        if "бензин" in benzik:
+            fuel = "бензин"
+        elif "дизель" in benzik:
+            fuel = "дизель"
+        elif "электро" in benzik:
+            fuel = "электро"
+        elif "гибрид" in benzik:
+            fuel = "гибрид"
+        if "TSI" in benzik:
+            engine = "TSI"
+        elif "TDI" in benzik:
+            engine = "TDI"
+        elif "MPI" in benzik:
+            engine = "MPI"
+        elif "CRDI" in benzik:
+            engine = "CRDI"
+        if "АКПП" in benzik:
+            transmission = "АКПП"
+        elif "МКПП" in benzik:
+            transmission = "МКПП"
+        elif "вариатор" in benzik:
+            transmission = "вариатор"
+        if "седан" in benzik:
+            car_body = "седан"
+        elif "хетчбек" in benzik:
+            car_body = "хетчбек"
+        elif "внедорожник" in benzik:
+            car_body = "внедорожник"
+        elif "универсал" in benzik:
+            car_body = "универсал"
+        elif "кабриолет" in benzik:
+            car_body = "кабриолет"
+        elif "микроавтобус" in benzik:
+            car_body = "микроавтобус"
+        elif "пикап" in benzik:
+            car_body = "пикап" 
+    #print(volume, fuel, transmission, engine, car_body)
+    #print(benzik)"""
+
+    file = open(f"data_bamper.csv", "a", encoding="utf-8", newline='')
+    writer = csv.writer(file)
+
+    writer.writerow(
+        (
+            artical,
+            marka,
+            model,
+            year,
+            fuel,
+            volume,
+            engine,
+            transmission,
+            car_body,
+            name_part,
+            info,
+            order,
+            price,
+            status
+        )
+    )
+    file.close()
     os.remove(f"{number}.html")
 os.remove("index.html")
 driver.close()
