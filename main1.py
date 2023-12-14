7a5d1cc156347cbe5285ef01724c9972c22692bf
Никита Молчанов <nylesszpg@gmail.com>
2023-12-14 12:57:31 +0300

    написал файл txt

--- main1.py
+++ main1.py
@@ -17,7 +17,7 @@ from PIL import Image, UnidentifiedImageError
 import time
 
 
-headers = {
+"""headers = {
     "Accept" : "application/json, text/javascript, */*; q=0.01",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
 }
@@ -27,15 +27,69 @@ url = "https://bamper.by/catalog/modeli/"
 req = requests.get(url, headers=headers)
 src = req.text
 #print(src)
-
 with open("index.html", "w", encoding="utf-8") as file:
     file.write(src)
+name_part= []
+with open("index.html", encoding="utf-8") as file:
+    src = file.read()
+f = open( '123.txt', 'w', encoding="utf-8" )
+soup = BeautifulSoup(src, "lxml")
+all_mark_models = soup.find_all("div", class_="row")
+for item in all_mark_models:
+    item = str(item.text).replace("  ","").replace("\n\n","")
+    # print(item)
+    f.write("%s\n" % item)
+f.close()
+with open('123.txt', 'r', encoding="utf-8" ) as file:
+    for line in file:
+        if "запчасти" in line:
+            line = line.replace("\n","").replace("запчасти для","")
+            name_part.append(line)
+#print(name_part)
+
+f = open( '1234.txt', 'w', encoding="utf-8" )
+for item in name_part:
+    f.write("%s\n" % item)
+f.close()""" 
+
+
+headers = {
+    "Accept" : "application/json, text/javascript, */*; q=0.01",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+}
+
+url = "https://bamper.by/catalog/modeli/"
+
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+#options.add_experimental_option('excludeSwitches', ['enable-automation'])
+#options.add_experimental_option('useAutomationExtension', False)
+options.add_argument('--ignore-certificate-errors')
+#options.add_argument("--proxy-server=31.204.2.182:9142")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+    'source': '''
+        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Array:
+        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Promise:
+        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Symbol:
+    '''
+})
+
+driver.get(url=url)
+time.sleep(2)
+
+with open("index.html", "w", encoding="utf-8") as file:
+    file.write(driver.page_source)
 
 with open("index.html", encoding="utf-8") as file:
     src = file.read()
 
-soup = BeautifulSoup(src, "lxml")
-all_mark_models = soup.find_all("div", class_="row")
-for item in all_mark_models:
-    #item = item.get('href')
-    print(item)
+soup = BeautifulSoup(src, 'html.parser')
+
+count = soup.find_all(class_="row").find_all("li")
+for item in count:
+    print(count)
+
+
