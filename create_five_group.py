18b7f360774cf64ce8c5d9ea074c93db0ca73d07
Никита Молчанов <nylesszpg@gmail.com>
2023-12-26 16:40:40 +0300

    ЗаРаБоТаЛо

--- create_five_group.py
+++ create_five_group.py
@@ -201,10 +201,10 @@ for item_text_model, item_href_model in model_need_list.items():
                     zapchast9000[markah] = modelh 
         os.remove(f"{item_text_model}.html") 
 
-with open("zapchas00_900", "a", encoding="utf-8") as file:
+with open("zapchas00_900.json", "a", encoding="utf-8") as file:
     json.dump(zapchast00_900, file, indent=4, ensure_ascii=False)
 
-with open("zapchast900_1200", "a", encoding="utf-8") as file:
+with open("zapchast900_1200.json", "a", encoding="utf-8") as file:
     json.dump(zapchast900_1200, file, indent=4, ensure_ascii=False)
 
 with open("zapchast1200_2400.json", "a", encoding="utf-8") as file:
