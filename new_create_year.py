7b4f452e6d9350744898bdbe4c1a4e63fdd1b0a0
Никита Молчанов <nylesszpg@gmail.com>
2024-01-11 16:52:19 +0300

    Делим по годам

---
+++ new_create_year.py
@@ -0,0 +1,155 @@
+import json
+from turtle import pd
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+import requests
+from bs4 import BeautifulSoup
+import os
+import shutil
+import csv
+from PIL import Image, UnidentifiedImageError
+import time
+
+
+headers = {
+    "Accept" : "application/json, text/javascript, */*; q=0.01",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+}
+
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+#options.add_experimental_option('useAutomationExtension', False)
+options.add_argument('--ignore-certificate-errors')
+options.add_argument("start-maximized") # // https://stackoverflow.com/a/26283818/1689770
+options.add_argument("enable-automation")#  // https://stackoverflow.com/a/43840128/1689770
+#options.add_argument("--headless")#  // only if you are ACTUALLY running headless
+options.add_argument("--no-sandbox")# //https://stackoverflow.com/a/50725918/1689770
+options.add_argument("--disable-dev-shm-usage")# //https://stackoverflow.com/a/50725918/1689770
+options.add_argument("--disable-browser-side-navigation")# //https://stackoverflow.com/a/49123152/1689770
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-infobars")# //https://stackoverflow.com/a/43840128/1689770
+options.add_argument("--enable-javascript")
+
+#options.add_argument("--proxy-server=31.204.2.182:9142")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+    'source': '''
+        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Array:
+        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Promise:
+        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Symbol:
+    '''
+})
+
+summa = 0
+black_list = []
+black_mark = []
+black_model = []
+
+file1 = open("black-list.txt", "r")
+while True:
+    # считываем строку
+    line = file1.readline()
+    line = line.replace("\n","").replace("'","").replace(" ","")
+    # прерываем цикл, если строка пустая
+    if not line:
+        break
+    # выводим строку
+    black_list.append(line)
+#print(black_list)
+# закрываем файл
+file1.close
+
+file1 = open("black-mark.txt", "r", encoding="utf-8")
+while True:
+    # считываем строку
+    line = file1.readline()
+    line = line.replace("\n","").replace("'","").replace(" ","")
+    # прерываем цикл, если строка пустая
+    if not line:
+        break
+    # выводим строку
+    black_mark.append(line)
+#print(black_list)
+
+# закрываем файл
+file1.close
+
+file1 = open("black-model.txt", "r", encoding="utf-8")
+while True:
+    # считываем строку
+    line = file1.readline()
+    line = line.replace("\n","").replace("'","").replace(" ","")
+    # прерываем цикл, если строка пустая
+    if not line:
+        break
+    # выводим строку
+    black_model.append(line)
+#print(black_list)
+
+
+
+zapchast00_1200_year = {}
+zapchast1200_year = {}
+n=1
+
+with open("zapchast1200.json", encoding="utf-8") as file:
+    srazy_parsim = json.load(file)
+
+for item_href_categories, count_page in srazy_parsim.items():
+    
+    item_href_categories = str(item_href_categories)
+    markah = item_href_categories[item_href_categories.find("marka")+6:item_href_categories.find("/model_")]
+    #print(markah)
+    modelh = item_href_categories[item_href_categories.find("model")+6:item_href_categories.find("/god_")]
+    print(markah, modelh)
+    for year in range(2012,2024):
+
+        url_zapchast = f"https://bamper.by/zchbu/marka_{markah}/model_{modelh}/god_{year}-{year}/price-ot_70/store_Y/?more=Y"
+        #print(url_zapchast)
+        driver.get(url=url_zapchast)
+        time.sleep(1)
+
+        with open(f"{count_page}.html", "w", encoding="utf-8") as file:
+            file.write(driver.page_source)
+
+        with open(f"{count_page}.html", encoding="utf-8") as file:
+            src = file.read()
+
+        soup = BeautifulSoup(src, 'html.parser')
+
+        count = soup.find_all("h5", class_="list-title js-var_iCount")
+        #print(count)
+        for item in count:
+            item = str(item)
+            if "<b>" in item:
+                #print(item)
+                num_page = item[item.find("<b>")+3: item.find("</b>")]
+                num_page = int(num_page.replace(" ",""))
+                summa = summa + num_page
+                if num_page > 0 and num_page < 1201:
+                    page = int(num_page / 20) + 1
+                    zapchast00_1200_year[url_zapchast] = page
+                else:
+                    page = int(num_page / 20) + 1
+                    zapchast1200_year[url_zapchast] = page
+
+        os.remove(f"{count_page}.html") 
+
+    with open("zapchast00_1200_year.json", "a", encoding="utf-8") as file:
+        json.dump(zapchast00_1200_year, file, indent=4, ensure_ascii=False)
+
+    with open("zapchast1200_year.json", "a", encoding="utf-8") as file:
+        json.dump(zapchast1200_year, file, indent=4, ensure_ascii=False)
+
+
+print(summa)
+
+a = input("Нажмите 1 и ENTER, чтобы закончить это сумасшествие")
