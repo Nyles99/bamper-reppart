a0d49f4efbeb00ac60786b9ce98db8f72d6db576
Никита Молчанов <nylesszpg@gmail.com>
2024-02-05 15:12:44 +0300

    разбил последние части

---
+++ new_create_year_price_last_destroy.py
@@ -0,0 +1,134 @@
+import json
+from turtle import pd
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+import requests
+from bs4 import BeautifulSoup
+import os
+import shutil
+import csv
+from PIL import Image, UnidentifiedImageError
+import time
+
+
+headers = {
+    "Accept" : "application/json, text/javascript, */*; q=0.01",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+}
+
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+#options.add_experimental_option('useAutomationExtension', False)
+options.add_argument('--ignore-certificate-errors')
+options.add_argument("start-maximized") # // https://stackoverflow.com/a/26283818/1689770
+options.add_argument("enable-automation")#  // https://stackoverflow.com/a/43840128/1689770
+#options.add_argument("--headless")#  // only if you are ACTUALLY running headless
+options.add_argument("--no-sandbox")# //https://stackoverflow.com/a/50725918/1689770
+options.add_argument("--disable-dev-shm-usage")# //https://stackoverflow.com/a/50725918/1689770
+options.add_argument("--disable-browser-side-navigation")# //https://stackoverflow.com/a/49123152/1689770
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-infobars")# //https://stackoverflow.com/a/43840128/1689770
+options.add_argument("--enable-javascript")
+
+#options.add_argument("--proxy-server=31.204.2.182:9142")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+    'source': '''
+        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Array:
+        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Promise:
+        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Symbol:
+    '''
+})
+
+summa = 0
+
+
+
+zapchast00_1200_last_destroy = {}
+zapchast1200_last_destroy = {}
+n=1
+
+with open("zapchast1200_year_price_one_year.json", encoding="utf-8") as file:
+    srazy_parsim = json.load(file)
+
+for item_href_categories, count_page in srazy_parsim.items():
+    href_zapchast = []
+    item_href_categories = str(item_href_categories)
+    """markah = item_href_categories[item_href_categories.find("marka")+6:item_href_categories.find("/model_")]
+    #print(markah)
+    modelh = item_href_categories[item_href_categories.find("model")+6:item_href_categories.find("/god_")]
+    print(markah, modelh)"""
+    try:
+        first_price = int(item_href_categories[item_href_categories.find("/price-ot_")+10:item_href_categories.find("/price-do_")])
+        second_price = int(item_href_categories[item_href_categories.find("/price-do_")+10:item_href_categories.find("/store_Y/")])
+    except Exception:
+        first_price = 500
+    if first_price == 500:
+        href_1 = item_href_categories[: item_href_categories.find("/price-ot_")+10] + "500/price-do_800/store_Y/?more=Y"
+        href_2 = item_href_categories[: item_href_categories.find("/price-ot_")+10] + "800/price-do_1000/store_Y/?more=Y"
+        href_3 = item_href_categories[: item_href_categories.find("/price-ot_")+10] + "1000/price-do_1300/store_Y/?more=Y"
+        href_4 = item_href_categories[: item_href_categories.find("/price-ot_")+10] + "1300/price-do_1600/store_Y/?more=Y"
+        href_5 = item_href_categories[: item_href_categories.find("/price-ot_")+10] + "1600/store_Y/?more=Y"
+    else:
+        period_price = (second_price - first_price) / 5
+        print(period_price)
+        href_1 = item_href_categories[: item_href_categories.find("/price-ot_")+10] + str(first_price) + "/price-do_" + str(first_price + period_price) + "/store_Y/?more=Y"
+        href_2 = item_href_categories[: item_href_categories.find("/price-ot_")+10] + str(first_price + period_price) + "/price-do_" + str(first_price + 2*period_price) + "/store_Y/?more=Y"
+        href_3 = item_href_categories[: item_href_categories.find("/price-ot_")+10] + str(first_price + 2*period_price) + "/price-do_" + str(first_price + 3*period_price) + "/store_Y/?more=Y"
+        href_4 = item_href_categories[: item_href_categories.find("/price-ot_")+10] + str(first_price + 3*period_price) + "/price-do_" + str(first_price + 4*period_price) + "/store_Y/?more=Y"
+        href_5 = item_href_categories[: item_href_categories.find("/price-ot_")+10] + str(first_price + 4*period_price) + "/price-do_" + str(second_price) + "/store_Y/?more=Y"
+    href_zapchast.append(href_1)
+    href_zapchast.append(href_2)
+    href_zapchast.append(href_3)
+    href_zapchast.append(href_4)
+    href_zapchast.append(href_5)
+
+    
+    for url_zapchast in href_zapchast:
+        print(url_zapchast)
+    
+        #print(url_zapchast)
+        try:
+            driver.get(url=url_zapchast)
+            time.sleep(1)
+
+            with open(f"{count_page}.html", "w", encoding="utf-8") as file:
+                file.write(driver.page_source)
+
+            with open(f"{count_page}.html", encoding="utf-8") as file:
+                src = file.read()
+
+            soup = BeautifulSoup(src, 'html.parser')
+
+            count = soup.find_all("h5", class_="list-title js-var_iCount")
+            #print(count)
+            for item in count:
+                item = str(item)
+                if "<b>" in item:
+                    #print(item)
+                    num_page = item[item.find("<b>")+3: item.find("</b>")]
+                    num_page = int(num_page.replace(" ",""))
+                    print(num_page)
+                    summa = summa + num_page
+                    page = int(num_page / 20) + 1
+                    zapchast00_1200_last_destroy[url_zapchast] = page
+
+            os.remove(f"{count_page}.html")
+        except Exception:
+            print("попробуй снова") 
+
+with open("zapchast00_1200_last_destroy.json", "a", encoding="utf-8") as file:
+    json.dump(zapchast00_1200_last_destroy, file, indent=4, ensure_ascii=False)
+
+
+print(summa)
+
+a = input("Нажмите 1 и ENTER, чтобы закончить это сумасшествие")
